57cf6b49aa3e16984bb592039126734a7a2b707d
hivi6 <89378957+hivi6@users.noreply.github.com>
2025-02-14 23:16:24 +0530

    refactor: Added an icon to basic skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import { FaBeer } from "react-icons/fa";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="text-red-900 text-9xl">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="h-screen flex items-center justify-center">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="text-red-900 border-red-900 border-2 text-9xl flex items-center justify-center"
+        >
+          <FaBeer />
+          <div>count is {count}</div>
         </button>
       </div>
     </>
